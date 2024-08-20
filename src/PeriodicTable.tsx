2535ab2eea51ac344539fca9fddefd6c341107db
Towster_ <79161644+TowsterBusiness@users.noreply.github.com>
2024-08-20 14:37:01 -0700

    Periodic Element gui

--- src/PeriodicTable.tsx
+++ src/PeriodicTable.tsx
@@ -7,6 +7,7 @@ import { PeriodicElementProps } from "./PeriodicElement";
 function App() {
   let grid = [];
   let elementNumber = 0;
+  let elementHash = new Map();
   for (let y = 0; y < 11; y++) {
     let row = [];
     for (let x = 0; x < 18; x++) {
@@ -17,15 +18,33 @@ function App() {
         highlight: false,
       };
 
+      let isSave = false;
       if (element.xpos - 1 == x && element.ypos - 1 == y) {
-        elementNumber++;
+        if (elementNumber == 55) {
+          elementNumber = 71;
+        } else if (elementNumber == 87) {
+          elementNumber = 103;
+        } else if (elementNumber == 117) {
+          elementNumber = 56;
+        } else if (elementNumber == 70) {
+          elementNumber = 88;
+        } else {
+          elementNumber++;
+        }
+        isSave = true;
       } else {
         props.element = null;
       }
 
-      row.push(
+      let elementHtml = (
         <PeriodicElement key={crypto.randomUUID()} {...props}></PeriodicElement>
       );
+
+      if (isSave) {
+        elementHash.set(props.element?.symbol, elementHtml);
+      }
+
+      row.push(elementHtml);
     }
     grid.push(
       <div key={y} className="grid-column">
