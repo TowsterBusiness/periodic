52c46a2cd344ae11eb67ad90924004026583c38f
Towster_ <79161644+TowsterBusiness@users.noreply.github.com>
2024-10-20 23:04:05 -0700

    easter eggs + addition

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,26 @@ import { ElementJson } from "./ElementDataTypes";
 import moleculeListFile from "./assets/molecule_list.json";
 import Suggestions from "./Suggestions";
 import MoleculeModel from "./MoleculeModel";
+import ReactMarkdown from "react-markdown";
 
 let previousChemicalList: Array<string>;
 let IPAdress: String;
+let textBoxText: String;
 let lastChemicalName: String = "";
 let lastChemicalTime: number = Number.MAX_VALUE;
+let finalPrompt: String = "";
 
 enum States {
   ATOMIC_MASS,
   MOLECULE_STRUCTURE,
+  CHAT_BOT,
+  EASTER_EGG,
+}
+
+enum ChatBotStates {
+  LOADING,
+  RESPONSE,
+  WAITING,
 }
 
 function App() {
@@ -25,18 +36,21 @@ function App() {
   const [moleculeList, setMoleculeList] = useState([""]);
   const [moleculeName, setMoleculeName] = useState("");
   const [state, setState] = useState(States.ATOMIC_MASS);
+  const [chatBotState, setChatBotState] = useState(ChatBotStates.WAITING);
+  const [chatBotOutput, setChatBotOutput] = useState("");
+  const [easterEggOutput, setEasterEggOutput] = useState(<></>);
   const refPeriodicTable: React.ForwardedRef<any> =
     React.createRef<HTMLDivElement | null>();
 
   // Future databasing program for: common formulas or molcules ect
-  // fetch("https://api.ipify.org?format=json")
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     IPAdress = data.ip;
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error fetching IP address:", error);
-  //   });
+  fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => {
+      IPAdress = data.ip;
+    })
+    .catch((error) => {
+      console.error("Error fetching IP address:", error);
+    });
 
   var textbox: HTMLInputElement = document.getElementById(
     "text-box"
@@ -44,6 +58,7 @@ function App() {
 
   var onInputHandler = (evt: any) => {
     var editedText: string = evt.target.value;
+    textBoxText = editedText;
 
     if (editedText.charAt(0) == "$") {
       setState(States.MOLECULE_STRUCTURE);
@@ -53,6 +68,31 @@ function App() {
       if (moleculeList.length > 1) {
         setMoleculeName(moleculeListTemp[0]);
       }
+    } else if (editedText.charAt(0) == "?") {
+      // setState(States.CHAT_BOT);
+      // let text = editedText.slice(1);
+      // finalPrompt = text;
+    } else if (editedText.charAt(0) == ":" && editedText.charAt(1) == ")") {
+      let text = editedText.slice(2).toLowerCase();
+      setState(States.EASTER_EGG);
+
+      if (text == ":)") {
+        setEasterEggOutput(<h1>Yippie!!!! 🎉</h1>);
+      } else if (text == ":(") {
+        setEasterEggOutput(<h1>Naur 😭😭😭😭</h1>);
+      } else if (text == "i love you") {
+        setEasterEggOutput(<h1>I love me too!</h1>);
+      } else if (text == "<3") {
+        setEasterEggOutput(<h1>{"<3 <3 <3 <3"}</h1>);
+      } else if (text == "good kid") {
+        setEasterEggOutput(
+          <h1>
+            <a href="https://www.youtube.com/watch?v=s1EnIwFnLyQ">peak?</a>
+          </h1>
+        );
+      } else if (text == "who made these") {
+        setEasterEggOutput(<h1>{"Alyssa :("}</h1>);
+      }
     } else {
       setState(States.ATOMIC_MASS);
       if (editedText == "") {
@@ -60,63 +100,71 @@ function App() {
         setFormula("Waiting...");
       }
 
-      let formula = atomicFormulaFromString(editedText);
-      if (formula != null) {
-        setFormula(formula);
-      }
-
-      // Chemical Number Check
-      elementFile["elements"].forEach((element: ElementJson) => {
-        if (editedText == element.number.toString()) {
-          setOutput(element.name);
+      if (RegExp("\\*|\\/|\\+|-", "g").test(editedText)) {
+        console.log("Using eval");
+        try {
+          setOutput(eval(editedText));
+          setFormula("");
+        } catch (error) {}
+      } else {
+        let formula = atomicFormulaFromString(editedText);
+        if (formula != null) {
+          setFormula(formula);
         }
-      });
 
-      var chemicalList: Array<string> = editedText
-        .replaceAll(new RegExp("\\(|\\)|[0-9]|\\.", "g"), "")
-        .split(new RegExp("(?=[A-Z])"));
-      console.log(chemicalList);
-
-      var elementList: Array<ElementJson> = [];
-
-      var moleculeWeight: number = atomicMassFromString(editedText);
-      console.log(calculatorFromString(editedText));
-
-      chemicalList.forEach((chemical) => {
-        var element: ElementJson | null = getElementBySymbol(chemical);
-        if (element != null) {
-          if (refPeriodicTable.current) {
-            refPeriodicTable.current?.triggerHighlight(element.number, true);
-          }
-          elementList.push(element);
-        }
-      });
-
-      if (previousChemicalList != null) {
-        previousChemicalList.forEach((pElement) => {
-          let hasElement: boolean = false;
-          chemicalList.forEach((cElement) => {
-            if (pElement == cElement) hasElement = true;
-          });
-          if (hasElement == false) {
-            refPeriodicTable.current?.triggerHighlight(
-              getElementBySymbol(pElement)?.number,
-              false
-            );
+        // Chemical Number Check
+        elementFile["elements"].forEach((element: ElementJson) => {
+          if (editedText == element.number.toString()) {
+            setOutput(element.name);
           }
         });
-      }
-      previousChemicalList = chemicalList;
 
-      setElementList(elementList);
-      setOutput((Math.round(moleculeWeight * 1000) / 1000).toString());
+        var chemicalList: Array<string> = editedText
+          .replaceAll(new RegExp("\\(|\\)|[0-9]|\\.", "g"), "")
+          .split(new RegExp("(?=[A-Z])"));
+        console.log(chemicalList);
 
-      console.log(Date.now() - lastChemicalTime);
-      if (Date.now() > lastChemicalTime + 3000 && lastChemicalName != "") {
-        // sendContent(IPAdress, lastChemicalName);
+        var elementList: Array<ElementJson> = [];
+
+        var moleculeWeight: number = atomicMassFromString(editedText);
+        console.log(calculatorFromString(editedText));
+
+        chemicalList.forEach((chemical) => {
+          var element: ElementJson | null = getElementBySymbol(chemical);
+          if (element != null) {
+            if (refPeriodicTable.current) {
+              refPeriodicTable.current?.triggerHighlight(element.number, true);
+            }
+            elementList.push(element);
+          }
+        });
+
+        if (previousChemicalList != null) {
+          previousChemicalList.forEach((pElement) => {
+            let hasElement: boolean = false;
+            chemicalList.forEach((cElement) => {
+              if (pElement == cElement) hasElement = true;
+            });
+            if (hasElement == false) {
+              refPeriodicTable.current?.triggerHighlight(
+                getElementBySymbol(pElement)?.number,
+                false
+              );
+            }
+          });
+        }
+        previousChemicalList = chemicalList;
+
+        setElementList(elementList);
+        setOutput((Math.round(moleculeWeight * 1000) / 1000).toString());
+
+        // console.log(Date.now() - lastChemicalTime);
+        // if (Date.now() > lastChemicalTime + 3000 && lastChemicalName != "") {
+        //   sendContent(IPAdress, lastChemicalName);
+        // }
+        // lastChemicalTime = Date.now();
+        // lastChemicalName = editedText;
       }
-      lastChemicalTime = Date.now();
-      lastChemicalName = editedText;
     }
   };
 
@@ -148,13 +196,53 @@ function App() {
     );
   };
 
+  const renderChatBot = () => {
+    if (chatBotState == ChatBotStates.WAITING) {
+      return <h3 id="chattext">...Waiting for a prompt</h3>;
+    } else if (chatBotState == ChatBotStates.LOADING) {
+      return <img src="src/assets/loadingAnimation.gif" alt="Loading..."></img>;
+    } else if (chatBotState == ChatBotStates.RESPONSE) {
+      return (
+        <p id="chattext">
+          <ReactMarkdown>{chatBotOutput}</ReactMarkdown>
+        </p>
+      );
+    }
+  };
+
+  const renderEasterEgg = () => {
+    return easterEggOutput;
+  };
+
+  const onSubmit = (evt: any) => {
+    evt.preventDefault();
+    console.log(`Submitted`);
+    if (state == States.CHAT_BOT) {
+      const fetchData = async () => {
+        try {
+          // Using Axios
+          setChatBotState(ChatBotStates.LOADING);
+          const response = await sendChatRequest(finalPrompt);
+          setChatBotState(ChatBotStates.RESPONSE);
+          setChatBotOutput(response); // Assuming we're interested in the title
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
+      fetchData();
+    } else if (state == States.ATOMIC_MASS) {
+      // sendContent(IPAdress, textBoxText);
+    }
+  };
+
   return (
     <div className="App">
       <h1>⚛️ Periodic ⚛️</h1>
       <p>Finds the atomic weights of atoms and molecules</p>
       <p>Ex. "(NH.4).3PO.4" would be (NH₄)₃PO₄</p>
       <p>Hint: use $ at the front to see many molecules</p>
-      <form id="main-input" className="noselect">
+      <form id="main-input" className="noselect" onSubmit={onSubmit}>
         <input
           type="text"
           id="text-box"
@@ -168,11 +256,18 @@ function App() {
 
       {state == States.ATOMIC_MASS ? renderAtomicMass() : <></>}
 
+      {state == States.CHAT_BOT ? renderChatBot() : <></>}
+
+      {state == States.EASTER_EGG ? renderEasterEgg() : <></>}
+
       <h6>
         All elements derived from{" "}
         <a href="https://github.com/Bowserinator/Periodic-Table-JSON/blob/master/PeriodicTableJSON.json">
           here
         </a>
+        <br />
+        All molecules from{" "}
+        <a href="https://github.com/OpenChemistry/molecules">here</a>
         <br />
         Socials: <span> </span>
         <a href="https://discord.gg/NnT5werabb">discord</a>
@@ -208,23 +303,25 @@ function getElementBySymbol(s: string): ElementJson | null {
 }
 
 function calculatorFromString(s: String): number {
-  let pointer = 0;
-
   let builder: Array<number | String> = [];
   let individualBuilder: String = "";
 
-  while (pointer < s.length) {
+  for (let pointer = 0; pointer < s.length; pointer++) {
     const c = s.charAt(pointer);
     if (RegExp("[0-9]").test(c)) {
       individualBuilder += c;
-    } else if (RegExp("*|/|+|-").test(c)) {
+    } else if (RegExp("\\*|\\/|\\+|-").test(c)) {
       builder.push(parseFloat(individualBuilder.toString()));
+      console.log("builder1", builder);
       individualBuilder = "";
       builder.push(c);
+      console.log("builder2", builder);
     }
     pointer++;
   }
   builder.push(parseFloat(individualBuilder.toString()));
+
+  console.log("builder", builder);
 
   for (let i = 0; i < builder.length; i++) {}
   return 0;
@@ -358,6 +455,35 @@ function sendContent(ip: String, content: String) {
     .catch((error) => {
       console.error("Error submitting data:", error);
     });
+}
+
+async function sendChatRequest(prompt: String): Promise<string> {
+  //TODO: Add checks for redundancy
+
+  const data = { prompt: prompt };
+
+  const response = await fetch("http://localhost:4000/chatbot", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Response from server:", data);
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error submitting data:", error);
+    });
+
+  return response.response;
 }
 
 export default App;
